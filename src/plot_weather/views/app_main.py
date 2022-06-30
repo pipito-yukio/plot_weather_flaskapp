6ff1d6eff460175b2ddc276a9b32654ad75e966f
pipito-yukio <33173429+pipito-yukio@users.noreply.github.com>
2022-06-30 14:46:14 +0900

    Added image acquisition request path for smart phones\n ,Optimized exception generation function for existing request path

--- src/plot_weather/views/app_main.py
+++ src/plot_weather/views/app_main.py
@@ -1,4 +1,6 @@
-from flask import abort, g, jsonify, render_template
+from http.client import responses
+
+from flask import abort, g, jsonify, render_template, request
 from plot_weather import (
     BAD_REQUEST_IMAGE_DATA,
     INTERNAL_SERVER_ERROR_IMAGE_DATA,
@@ -13,7 +15,7 @@ from plot_weather.plotter.plotterweather import gen_plotimage
 from werkzeug.exceptions import BadRequest
 
 APP_ROOT = app.config["APPLICATION_ROOT"]
-CODE_BAD_REQUEST, CODE_INTERNAL_SERVER_ERROR = 400, 500
+CODE_BAD_REQUEST, CODE_FORBIDDEN, CODE_INTERNAL_SERVER_ERROR = 400, 403, 500
 MSG_TITLE_SUFFIX = app.config["TITLE_SUFFIX"]
 MSG_STR_TODAY = app.config["STR_TODAY"]
 
@@ -54,7 +56,7 @@ def index():
             device_name=WEATHER_CONF["DEVICE_NAME"],
             start_date=WEATHER_CONF["STA_YEARMONTH"],
         )
-        app_logger.debug("yearMonthList:{}".format(yearMonthList))
+        app_logger.debug(f"yearMonthList:{yearMonthList}")
         # 本日データプロット画像取得
         imgBase64Encoded = gen_plotimage(conn, logger=app_logger)
     except Exception as exp:
@@ -93,7 +95,7 @@ def getTodayImage():
         imgBase64Encoded = gen_plotimage(conn, year_month=None, logger=app_logger)
     except Exception as exp:
         app_logger.error(exp)
-        return _create_error_response(CODE_INTERNAL_SERVER_ERROR)
+        return _create_image_error_response(CODE_INTERNAL_SERVER_ERROR)
 
     return _create_image_response(imgBase64Encoded)
 
@@ -106,7 +108,7 @@ def getMonthImage(yearmonth):
     :return: jSON形式(matplotlibでプロットした画像データ(形式: png)のbase64エンコード済み文字列)
          (出力内容) jSON('data:image/png;base64,... base64encoded data ...')
     """
-    app_logger.debug("yearmonth: {}".format(yearmonth))
+    app_logger.debug(f"yearmonth: {yearmonth}")
     try:
         # リクエストパラメータの妥当性チェック: "YYYY-mm" + "-01"
         chk_yyyymmdd = yearmonth + "-01"
@@ -118,19 +120,29 @@ def getMonthImage(yearmonth):
     except DateFormatError as dfe:
         # BAD Request
         app_logger.warning(dfe)
-        return _create_error_response(CODE_BAD_REQUEST)
+        return _create_image_error_response(CODE_BAD_REQUEST)
     except Exception as exp:
         # ここにくるのはDBエラー・バグなど想定
         app_logger.error(exp)
-        return _create_error_response(CODE_INTERNAL_SERVER_ERROR)
+        return _create_image_error_response(CODE_INTERNAL_SERVER_ERROR)
 
     return _create_image_response(imgBase64Encoded)
 
 
-@app.route("/plot_weather/getcurrenttimedata", methods=["GET"])
-def getcurrenttimedata():
-    """現在時刻での最新の気象データを取得する"""
-    app_logger.debug("getcurrenttimedata()")
+@app.route("/plot_weather/getlastdataforphone", methods=["GET"])
+def getLastDataForPhone():
+    """最新の気象データを取得する (スマートホン専用)"""
+    app_logger.debug("getlastdataforphone()")
+    headers = request.headers
+    app_logger.debug(f"headers: {headers}")
+    # トークン必須
+    token_value = app.config.get("HEADER_REQUEST_PHONE_TOKEN_VALUE")
+    req_token_value = headers.get(app.config.get("HEADER_REQUEST_PHONE_TOKEN_KEY"))
+    if req_token_value != token_value:
+        # トークンが一致しなければエラー画面を返却 ※アプリからは同じトークンがセットされている
+        app_logger.warning("Invalid request token!")
+        return abort(CODE_FORBIDDEN)
+
     try:
         conn = get_dbconn()
         # 現在時刻時点の最新の気象データ取得
@@ -140,21 +152,82 @@ def getcurrenttimedata():
         )
     except Exception as exp:
         app_logger.error(exp)
-        return _create_error_response(CODE_INTERNAL_SERVER_ERROR)
+        abort(CODE_INTERNAL_SERVER_ERROR, description=str(exp))
 
-    return _create_currtimedatae_response(
+    return _response_lastdataforphone(
         measurement_time, temp_out, temp_in, humid, pressure
     )
 
 
+@app.route("/plot_weather/gettodayimageforphone", methods=["GET"])
+def getTodayImageForPhone():
+    """本日データ取得リクエスト (スマートホン専用)
+
+    :return: jSON形式(matplotlibでプロットした画像データ(形式: png)のbase64エンコード済み文字列)
+         (出力内容) jSON('data:image/png;base64,... base64encoded data ...')
+    """
+    app_logger.debug("getTodayImageForPhone()")
+    headers = request.headers
+    app_logger.debug(f"headers: {headers}")
+    # トークン必須
+    token_value = app.config.get("HEADER_REQUEST_PHONE_TOKEN_VALUE")
+    req_token_value = headers.get(app.config.get("HEADER_REQUEST_PHONE_TOKEN_KEY"))
+    if req_token_value != token_value:
+        app_logger.warning("Invalid request token!")
+        return abort(CODE_FORBIDDEN)
+
+    try:
+        # ヘッダーに表示領域サイズ([width]x[height])をつけてくる
+        # ※1.トークンチェックを通過しているのでセットされている前提で処理
+        # ※2.途中でエラー (Androidアプリ側のBUG) ならExceptionで補足されJSONでメッセージが返却される
+        img_size = headers.get(app.config.get("HEADER_REQUEST_IMAGE_SIZE_KEY"))
+        app_logger.debug(f"Phone imgSize: {img_size}")
+        img_wd, img_ht = 0, 0
+        if img_size is not None:
+            sizes = img_size.split("x")
+            img_wd = int(sizes[0])
+            img_ht = int(sizes[1])
+        app_logger.debug(f"imgWd: {img_wd} imgHt: {img_ht}")
+
+        conn = get_dbconn()
+        imgBase64Encoded = gen_plotimage(
+            conn,
+            width_pixel=(None if img_wd == 0 else img_wd),
+            height_pixel=(None if img_ht == 0 else img_ht),
+            year_month=None,
+            logger=app_logger,
+        )
+    except Exception as exp:
+        app_logger.error(exp)
+        abort(CODE_INTERNAL_SERVER_ERROR, description=str(exp))
+
+    return _response_todayimageforphone(imgBase64Encoded)
+
+
 def _create_image_response(img_src):
+    """画像レスポンスを返却する (JavaScript用)"""
     resp_obj = {"status": "success"}
     resp_obj["data"] = {"img_src": img_src}
     return jsonify(resp_obj)
 
 
-def _create_currtimedatae_response(mesurement_time, temp_out, temp_in, humid, pressure):
-    resp_obj = {"status": "success"}
+def _create_image_error_response(err_code):
+    """エラー画像レスポンスを返却する (JavaScript用)"""
+    resp_obj = {"status": "error", "code": err_code}
+    if err_code == CODE_BAD_REQUEST:
+        resp_obj["data"] = {"img_src": BAD_REQUEST_IMAGE_DATA}
+    else:
+        resp_obj["data"] = {"img_src": INTERNAL_SERVER_ERROR_IMAGE_DATA}
+    response = jsonify(resp_obj)
+    # レスポンスコードを指定されたエラーコードで上書きする
+    response.status_code = err_code
+    return response
+
+
+def _response_lastdataforphone(mesurement_time, temp_out, temp_in, humid, pressure):
+    """気象データの最終レコードを返却する (スマホアプリ用)"""
+    resp_obj = {}
+    resp_obj["status"] = {"code": 0, "message": "OK"}
     resp_obj["data"] = {
         "measurement_time": mesurement_time,
         "temp_out": temp_out,
@@ -165,10 +238,22 @@ def _create_currtimedatae_response(mesurement_time, temp_out, temp_in, humid, pr
     return jsonify(resp_obj)
 
 
-def _create_error_response(err_code):
-    resp_obj = {"status": "error", "code": err_code}
-    if err_code == CODE_BAD_REQUEST:
-        resp_obj["data"] = {"img_src": BAD_REQUEST_IMAGE_DATA}
-    else:
-        resp_obj["data"] = {"img_src": INTERNAL_SERVER_ERROR_IMAGE_DATA}
+def _response_todayimageforphone(img_src):
+    """本日データの気象データ画像を返却する (スマホアプリ用)"""
+    resp_obj = {}
+    resp_obj["status"] = {"code": 0, "message": "OK"}
+    resp_obj["data"] = {"img_src": img_src}
     return jsonify(resp_obj)
+
+
+def _error_response_forphone(error):
+    """テキストのエラーレスポンスを返却するする (スマホアプリ用)"""
+    resp_obj = {}
+    resp_obj["status"] = {"code": error.code, "message": error.description}
+    return jsonify(resp_obj)
+
+
+@app.errorhandler(CODE_BAD_REQUEST)
+@app.errorhandler(CODE_INTERNAL_SERVER_ERROR)
+def error_handler(error):
+    return _error_response_forphone(error), error.code
