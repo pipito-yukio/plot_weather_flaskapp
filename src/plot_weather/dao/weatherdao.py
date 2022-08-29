d6471d4c898fe7a1f881438607369c04e388b8bd
pipito-yukio <33173429+pipito-yukio@users.noreply.github.com>
2022-08-29 17:05:22 +0900

    Change database SQLite to PostgreSQL

--- src/plot_weather/dao/weatherdao.py
+++ src/plot_weather/dao/weatherdao.py
@@ -1,81 +1,81 @@
-import os
+import logging
+from datetime import date
+from io import StringIO
 
 from ..db.sqlite3conv import strdate2timestamp
-from ..util.dateutil import nextYearMonth
+from ..util.dateutil import addDayToString, nextYearMonth
 
 """ 気象データDAOクラス """
 
 HEADER_WEATHER = '"did","measurement_time","temp_out","temp_in","humid","pressure"'
 HEADER_DEVICE = '"id","name"'
 
-my_home = os.environ.get("HOME", "/home/yukio")
-weather_db = os.environ.get("PATH_WEATHER_DB", my_home + "/db/weather.db")
-
 
 class WeatherDao:
 
     _QUERY_WEATHER_LASTREC = """
 SELECT
-  MAX(measurement_time)
-  , strftime('%Y-%m-%d %H:%M', measurement_time, 'unixepoch', 'localtime')
+  to_char(measurement_time,'YYYY-MM-DD HH24:MI') as measurement_time
   , temp_out, temp_in, humid, pressure
 FROM
-  t_weather
+  weather.t_weather
 WHERE
-  did=(SELECT id FROM t_device WHERE name=?);
+  did=(SELECT id FROM weather.t_device WHERE name=%(name)s)
+  AND
+  measurement_time = (SELECT max(measurement_time) FROM weather.t_weather);
 """
 
     _QUERY_GROUPBY_DAYS = """
 SELECT
-  date(measurement_time, 'unixepoch', 'localtime') 
+  to_char(measurement_time, 'YYYY-MM-DD') as groupby_days
 FROM
-  t_weather
+  weather.t_weather
 WHERE
-  did=(SELECT id FROM t_device WHERE name=?)
+  did=(SELECT id FROM weather.t_device WHERE name=%(name)s)
   AND
-  date(measurement_time, 'unixepoch', 'localtime') >= ?
-GROUP BY date(measurement_time, 'unixepoch', 'localtime')
-ORDER BY date(measurement_time, 'unixepoch', 'localtime');
+  to_char(measurement_time, 'YYYY-MM-DD') >= '%s(groupby_days)s'
+GROUP BY to_char(measurement_time, 'YYYY-MM-DD')
+ORDER BY to_char(measurement_time, 'YYYY-MM-DD');
     """
 
     _QUERY_GROUPBY_MONTHS = """
 SELECT
-  strftime('%Y-%m', measurement_time, 'unixepoch', 'localtime') 
+  to_char(measurement_time, 'YYYY-MM') as groupby_months
 FROM
-  t_weather
+  weather.t_weather
 WHERE
-  did=(SELECT id FROM t_device WHERE name=?)
+  did=(SELECT id FROM weather.t_device WHERE name=%(name)s)
   AND
-  strftime('%Y-%m', measurement_time, 'unixepoch', 'localtime') >= ?
-  GROUP BY strftime('%Y-%m', measurement_time, 'unixepoch', 'localtime')
-  ORDER BY strftime('%Y-%m', measurement_time, 'unixepoch', 'localtime') DESC;
+  to_char(measurement_time, 'YYYY-MM') >= %(groupby_months)s
+  GROUP BY to_char(measurement_time, 'YYYY-MM')
+  ORDER BY to_char(measurement_time, 'YYYY-MM') DESC;
 """
 
     _QUERY_TODAY_DATA = """
 SELECT
-   did, datetime(measurement_time, 'unixepoch', 'localtime')
+   did, to_char(measurement_time,'YYYY-MM-DD HH24:MI') as measurement_time
    , temp_out, temp_in, humid, pressure
 FROM
-   t_weather
+   weather.t_weather
 WHERE
-   did=(SELECT id FROM t_device WHERE name=?)
+   did=(SELECT id FROM weather.t_device WHERE name=%(name)s)
    AND
-   measurement_time >= strftime('%s', date('now'), '-9 hours')
+   measurement_time >= to_timestamp(%(today)s, 'YYYY-MM-DD HH24:MI:SS')
 ORDER BY did, measurement_time;
 """
 
-    _QUERY_MONTH_DATA = """
+    _QUERY_RANGE_DATA = """
 SELECT
-   did, datetime(measurement_time, 'unixepoch', 'localtime')
+   did, to_char(measurement_time,'YYYY-MM-DD HH24:MI') measurement_time
    , temp_out, temp_in, humid, pressure
 FROM
-   t_weather
+   weather.t_weather
 WHERE
-   did=(SELECT id FROM t_device WHERE name=?)
+   did=(SELECT id FROM weather.t_device WHERE name=%(name)s)
    AND (
-     measurement_time >= strftime('%s', ?, '-9 hours')
+     measurement_time >= to_timestamp(%(from_date)s, 'YYYY-MM-DD HH24::MI:SS')
      AND
-     measurement_time < strftime('%s', ?, '-9 hours')
+     measurement_time < to_timestamp(%(to_next_date)s, 'YYYY-MM-DD HH24:MI:SS')
    )
 ORDER BY did, measurement_time;
 """
@@ -83,6 +83,10 @@ ORDER BY did, measurement_time;
     def __init__(self, conn, logger=None):
         self.conn = conn
         self.logger = logger
+        if self.logger is not None:
+            self.logger_debug = (self.logger.getEffectiveLevel() <= logging.DEBUG)
+        else:
+            self.logger_debug = False
 
     def getLastData(self, device_name):
         """観測デバイスの最終レコードを取得する
@@ -93,47 +97,40 @@ ORDER BY did, measurement_time;
         Returns:
           tuple: (measurement_time[%Y %m %d %H %M], temp_out, temp_in, humid, pressure)
         """
-        cursor = self.conn.cursor()
-        try:
-            cursor.execute(self._QUERY_WEATHER_LASTREC, (device_name,))
+        with self.conn.cursor() as cursor:
+            cursor.execute(self._QUERY_WEATHER_LASTREC, {'name': device_name})
             row = cursor.fetchone()
-            if self.logger is not None:
+            if self.logger_debug:
                 self.logger.debug("row: {}".format(row))
-        finally:
-            cursor.close()
-        # remove first column: MAX(measurement_time)
-        return row[1:]
 
-    def _getDateGroupByList(self, qrouping_sql, device_name, start_date):
+        return row
+
+    def _getDateGroupByList(self, qrouping_sql, device_name, start_date, groupby_name='groupby_days'):
         """観測デバイスのグルーピングSQLに対応した日付リストを取得する
 
         Args:
             qrouping_sql str: グルーピングSQL
             device_name str: 観測デバイス名
             start_date str: 検索開始日付
+            groupby_name: 'groupby_days' | 'groupby_months'
 
         Returns:
           list: 文字列の日付 (年月 | 年月日)
         """
+        if self.logger_debug:
+            self.logger.debug("{}, {}".format(device_name, start_date))
         # Check start_date
         strdate2timestamp(start_date)
-        cursor = self.conn.cursor()
-        try:
-            cursor.execute(
-                qrouping_sql,
-                (
-                    device_name,
-                    start_date,
-                ),
-            )
+
+        with self.conn.cursor() as cursor:
+            cursor.execute(qrouping_sql, {'name': device_name, groupby_name: start_date})
             # fetchall() return tuple list [(?,), (?,), ..., (?,)]
             tupledlist = cursor.fetchall()
-            if self.logger is not None:
+            if self.logger_debug:
                 self.logger.debug("tupledlist: {}".format(tupledlist))
             # tuple -> list
             result = [item for (item,) in tupledlist]
-        finally:
-            cursor.close()
+
         return result
 
     def getGroupbyDays(self, device_name, start_date):
@@ -147,7 +144,7 @@ ORDER BY did, measurement_time;
             list[str]: 年月日リスト(%Y-%m-%d)
         """
         return self._getDateGroupByList(
-            self._QUERY_GROUPBY_DAYS, device_name, start_date
+            self._QUERY_GROUPBY_DAYS, device_name, start_date, groupby_name="groupby_days"
         )
 
     def getGroupbyMonths(self, device_name, start_date):
@@ -161,62 +158,76 @@ ORDER BY did, measurement_time;
             list[str]: 降順の年月リスト(%Y-%m)
         """
         return self._getDateGroupByList(
-            self._QUERY_GROUPBY_MONTHS, device_name, start_date
+            self._QUERY_GROUPBY_MONTHS, device_name, start_date, groupby_name="groupby_months"
         )
 
-    def _toWeatherCSVList(self, tupledlist, require_header=True):
+    def _csvToStringIO(self, tupledlist, require_header=True):
+        str_buffer = StringIO()
         if require_header:
-            result_list = [HEADER_WEATHER]
-            for (did, m_time, temp_in, temp_out, humid, pressure) in tupledlist:
-                result_list.append(
-                    f'{did},"{m_time}",{temp_in},{temp_out},{humid},{pressure}'
-                )
-        else:
-            result_list = [
-                f'{did},"{m_time}",{temp_in},{temp_out},{humid},{pressure}'
-                for (did, m_time, temp_in, temp_out, humid, pressure) in tupledlist
-            ]
-        return result_list
+            str_buffer.write(HEADER_WEATHER+"\n")
+          
+        for (did, m_time, temp_in, temp_out, humid, pressure) in tupledlist:
+            line = f'{did},"{m_time}",{temp_in},{temp_out},{humid},{pressure}\n'
+            str_buffer.write(line)
+       
+        # StringIO need Set first position
+        str_buffer.seek(0)
+        return str_buffer
 
-    def getTodayData(self, device_name, output_string=False, require_header=True):
-        cursor = self.conn.cursor()
-        try:
-            cursor.execute(self._QUERY_TODAY_DATA, (device_name,))
+    def getTodayData(self, device_name, today='now', require_header=True):
+        if today == 'now':
+            s_today = date.today().strftime('%Y-%m-%d')
+        else:
+            s_today = today
+        if self.logger_debug:    
+            self.logger.debug("device_name: {}, today: {}".format(device_name, s_today))
+
+        with self.conn.cursor() as cursor:
+            cursor.execute(self._QUERY_TODAY_DATA, {'name': device_name, 'today': s_today})
             tupledlist = cursor.fetchall()
-            if self.logger is not None:
-                self.logger.debug("tupledlist size: {}".format(len(tupledlist)))
-            # [tuple, ...] -> list
-            csvList = self._toWeatherCSVList(tupledlist, require_header)
-        finally:
-            cursor.close()
-        if output_string:
-            return "\n".join(csvList)
-        else:
-            return csvList
+            if self.logger_debug:
+                self.logger.debug("tupledlist length: {}".format(len(tupledlist)))
 
-    def getMonthData(
-        self, device_name, s_year_month, output_string=False, require_header=True
-    ):
+        # [tuple, ...] -> StringIO buffer
+        return self._csvToStringIO(tupledlist, require_header)
+
+    def getMonthData(self, device_name, s_year_month, require_header=True):
         s_start = s_year_month + "-01"
         s_end_exclude = nextYearMonth(s_start)
-        cursor = self.conn.cursor()
-        try:
-            cursor.execute(
-                self._QUERY_MONTH_DATA,
-                (
-                    device_name,
-                    s_start,
-                    s_end_exclude,
-                ),
+        if self.logger_debug:
+            self.logger.debug("device_name: {}, from_date: {}, to_next_date: {}".format(
+                device_name, s_start, s_end_exclude))
+
+        with self.conn.cursor() as cursor:
+            cursor.execute(self._QUERY_RANGE_DATA, {
+                    'name': device_name,
+                    'from_date': s_start,
+                    'to_next_date': s_end_exclude,
+                }
             )
             tupledlist = cursor.fetchall()
-            if self.logger is not None:
-                self.logger.debug("tupledlist size: {}".format(len(tupledlist)))
-            # [tuple, ...] -> list
-            csvList = self._toWeatherCSVList(tupledlist, require_header)
-        finally:
-            cursor.close()
-        if output_string:
-            return "\n".join(csvList)
-        else:
-            return csvList
+            if self.logger_debug:
+                self.logger.debug("tupledlist length: {}".format(len(tupledlist)))
+
+        # [tuple, ...] -> StringIO buffer
+        return self._csvToStringIO(tupledlist, require_header)
+
+    def getFromToRangeData(self, device_name, from_date, to_date, require_header=True):
+        s_end_exclude = addDayToString(to_date)
+        if self.logger_debug:
+            self.logger.debug("device_name: {}, from_date: {}, to_next_date: {}".format(
+                device_name, from_date, s_end_exclude))
+
+        with self.conn.cursor() as cursor:
+            cursor.execute(self._QUERY_RANGE_DATA, {
+                    'name': device_name,
+                    'from_date': from_date,
+                    'to_next_date': s_end_exclude,
+                }
+            )
+            tupledlist = cursor.fetchall()
+            if self.logger_debug:
+                self.logger.debug("tupledlist length: {}".format(len(tupledlist)))
+
+        # [tuple, ...] -> StringIO buffer
+        return self._csvToStringIO(tupledlist, require_header)
