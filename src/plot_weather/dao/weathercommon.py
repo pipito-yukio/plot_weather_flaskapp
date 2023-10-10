47b66c468d135d00f77a42ec57476a2dd64ef432
yukio yoshida <33173429+pipito-yukio@users.noreply.github.com>
2023-10-10 17:29:40 +0900

    Remove unused configure

--- src/plot_weather/dao/weathercommon.py
+++ src/plot_weather/dao/weathercommon.py
@@ -6,4 +6,3 @@ _base_dir = os.path.abspath(os.path.dirname(__file__))
 _conf_path = os.path.join(_base_dir, "conf")
 
 PLOT_CONF = FU.read_json(os.path.join(_conf_path, "plot_weather.json"))
-WEATHER_CONF = FU.read_json(os.path.join(_conf_path, "weather.json"))
